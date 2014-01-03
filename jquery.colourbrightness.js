2be41783c24614674e1efc83ae4af904a4afbabd
Lasha Krikheli <lashakrikheli@burrito.local>
2014-01-03 00:32:43 -0500

    Rewrote plugin to support command separated jQuery selectors

--- jquery.colourbrightness.js
+++ jquery.colourbrightness.js
@@ -10,34 +10,51 @@
  */
 
 (function($){
-  $.fn.colourBrightness = function(){
-    var r,g,b,brightness,
-        colour = this.css("background-color");
+    $.colourBrightness = function(el){
+        var base = this;
+        
+        base.$el = $(el);
+        base.el = el;
+        
+        base.$el.data("colourBrightness", base);
+        
+        base.init = function(){
+          var r,g,b,brightness,
+              colour = base.$el.css("background-color");
 
-    if (colour.match(/^rgb/)) {
-      colour = colour.match(/rgba?\(([^)]+)\)/)[1];
-      colour = colour.split(/ *, */).map(Number);
-      r = colour[0];
-      g = colour[1];
-      b = colour[2];
-    } else if ('#' == colour[0] && 7 == colour.length) {
-      r = parseInt(colour.slice(1, 3), 16);
-      g = parseInt(colour.slice(3, 5), 16);
-      b = parseInt(colour.slice(5, 7), 16);
-    } else if ('#' == colour[0] && 4 == colour.length) {
-      r = parseInt(colour[1] + colour[1], 16);
-      g = parseInt(colour[2] + colour[2], 16);
-      b = parseInt(colour[3] + colour[3], 16);
-    }
+          if (colour.match(/^rgb/)) {
+            colour = colour.match(/rgba?\(([^)]+)\)/)[1];
+            colour = colour.split(/ *, */).map(Number);
+            r = colour[0];
+            g = colour[1];
+            b = colour[2];
+          } else if ('#' == colour[0] && 7 == colour.length) {
+            r = parseInt(colour.slice(1, 3), 16);
+            g = parseInt(colour.slice(3, 5), 16);
+            b = parseInt(colour.slice(5, 7), 16);
+          } else if ('#' == colour[0] && 4 == colour.length) {
+            r = parseInt(colour[1] + colour[1], 16);
+            g = parseInt(colour[2] + colour[2], 16);
+            b = parseInt(colour[3] + colour[3], 16);
+          }
 
-    brightness = (r * 299 + g * 587 + b * 114) / 1000;
+          brightness = (r * 299 + g * 587 + b * 114) / 1000;
 
-    if (brightness < 125) {
-      // white text
-      this.removeClass("light").addClass("dark");
-    } else {
-      // black text
-      this.removeClass("dark").addClass("light");
-    }
-  }
+          if (brightness < 125) {
+            // white text
+            base.$el.removeClass("light").addClass("dark");
+          } else {
+            // black text
+            base.$el.removeClass("dark").addClass("light");
+          }
+        };
+        base.init();
+    };
+    
+    $.fn.colourBrightness = function(){
+        return this.each(function(){
+            (new $.colourBrightness(this));
+        });
+    };
+    
 })(jQuery);
